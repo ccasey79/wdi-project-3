55530f41e3331320d5b5656e4f66cac8a534544f
christian <christiancasey969@gmail.com>
2016-09-09 11:41:59 +0100

    Deploying to heroku

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 dbURIs = {
   test: "mongodb://localhost/project3-test",
   development: "mongodb://localhost/project3-test",
-  production: process.env.MONGOLAB_URI || "mongodb://localhost/project3-test"
+  production: process.env.MONGODB_URI || "mongodb://localhost/project3-test"
 }
 
 module.exports = function(env) {
