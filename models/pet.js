f5bba16996ea62950137cf0966ba2fc04a120c28
christian <christiancasey969@gmail.com>
2016-09-10 15:33:07 +0100

    public templates and pets htmls added

--- models/pet.js
+++ models/pet.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var petSchema = new mongoose.Schema({
   image: { type: String, required: true },
   name: { type: String, required: true, unique: true },
-  breed: { type: String },
+  breed: { type: String }
 
 
 }); 
---
+++ public/js/app.js
@@ -0,0 +1,46 @@
+angular 
+  .module('wdi-project-3', ["ui.router", "ngResource"])
+  .config(Router);
+
+Router.$inject = ['$stateProvider', '$urlRouterProvider'];
+function Router($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('register', {
+      url: '/register',
+      templateUrl: '/templates/register.html'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'templates/login.html' 
+    })
+    .state("petsIndex", {
+      url: "/pets",
+      templateUrl: "/templates/pets/index.html"
+    })
+    .state("petsNew", {
+      url: "/pets/:id",
+      templateUrl: "templates/pets/new.html"
+    })
+    .state("petsShow", {
+      url: "/pets/:id",
+      templateUrl: "/templates/pets/show.html"
+    })
+    .state("petsEdit", {
+      url: "/pets/:id/edit",
+      templateUrl: "/templates/pets/edit.html"
+    })
+
+  $urlRouterProvider.otherwise("/login");  
+}
+
+
+
+
+
+
+
+
+
+
+
+
